6564d762eaf06ca7ec33285c5626a8e8d53d2a85
Kevin Avila <kevin.avila@avantgarde.de>
2024-03-11 12:35:01 +0100

    Initial commit - Add Hello World\!

---
+++ hello-world/main.rs
@@ -0,0 +1,9 @@
+// The main function is the entrypoint for every rust program
+fn main() {
+    // The default indent is 4-space, not tab.
+    // The println! is a macro, not a function.
+    println!("Hello, world!")
+}
+
+// Compiling and running are two different steps.
+// To compile, use `rustc main.rs`, to run, use `./main`
